e1a5b9a0fecc703c308504c9e25a76a4a6c550a2
Botirov Boburbek <74315658+Boburshoh-oss@users.noreply.github.com>
2023-05-08 14:26:34 +0000

    api url changed

--- src/services/requests.ts
+++ src/services/requests.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getHeaders } from './getHeaders';
 
-export const BASE_URL = 'http://tozauz-api.saminjonov.uz/api/v1/';
+export const BASE_URL = 'https://api.tozauz.uz/api/v1/';
 
 export const requests = async (method: string = 'GET', path: string, data?: any, withAuth: boolean = true) => {
     if (withAuth) {
