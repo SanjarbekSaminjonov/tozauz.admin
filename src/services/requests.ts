b10c1025637d4f11ace6aa233959d6cfb5930cab
SanjarbekSaminjonov <sanjarbeksaminjonovv@gmail.com>
2023-06-27 16:59:27 +0500

    401 auto logout

--- src/services/requests.ts
+++ src/services/requests.ts
@@ -1,17 +1,26 @@
 import axios from 'axios';
 import { getHeaders } from './getHeaders';
 
-// export const BASE_URL = 'http://tozauz-api.saminjonov.uz/api/v1/';
 export const BASE_URL = 'https://api.tozauz.uz/api/v1/';
 
 export const requests = async (method: string = 'GET', path: string, data?: any, withAuth: boolean = true) => {
     if (withAuth) {
-        return axios({
-            method: method,
-            url: BASE_URL + path,
-            data: data,
-            headers: getHeaders()
-        });
+        try {
+            return await axios({
+                method: method,
+                url: BASE_URL + path,
+                data: data,
+                headers: getHeaders()
+            });
+        }
+        catch (error: any) {
+            if (error.response.status === 401) {
+                localStorage.removeItem('token');
+                localStorage.removeItem('admin');
+                window.location.href = '/login';
+            }
+            return {"data": []};
+        }
     } else {
         return axios({
             method: method,
